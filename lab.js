512e2098b301348cc7330e08da8219d6090b55bf
Nilber Remon <nilberremon@gmail.com>
2019-10-08 21:19:14 -0400

    Problem 1 Complete

--- lab.js
+++ lab.js
@@ -12,21 +12,22 @@ var applesObj = {
 
 // a. Set eveAppleCount equal to the number of apples that Eve has
 
-let eveAppleCount /* = Your code here */
+let eveAppleCount = applesObj["Eve"]
+//console.log(eveAppleCount)
 
-// assert(eveAppleCount === 4)
+assert(eveAppleCount === 4)
 
 // b. Change the number of apples that Adam  has to 4
 
-// Your code here
+applesObj["Adam"] = 4
 
-//assert(applesObj["Adam"] === 4)
+assert(applesObj["Adam"] === 4)
 
 // c. Set calAndDanAppleCount equal to how many apples Cal and Dan have combined
 
-let calAndDanAppleCount /*= Your code here */
+let calAndDanAppleCount = applesObj["Cal"] + applesObj["Dan"]
 
-//assert(calAndDanAppleCount === 8)
+assert(calAndDanAppleCount === 8)
 
 // d. Set all the values in applesObj to 0
 
